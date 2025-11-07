fd9f35465a44052322a95cd0d72225f3f02a4865
n_chin <nicoli.ihc875@gmail.com>
2025-11-07 19:35:40 -0300

    Add age verification script in atividade1.py

---
+++ atividade1.py
@@ -0,0 +1,11 @@
+# atividade1.py - Verificador de Maioridade
+
+nome = input("Qual é o seu nome? ")
+idade_texto = input("Qual é a sua idade? ")
+idade_numero = int(idade_texto)
+
+# Lógica
+if idade_numero >= 18:
+    print(f"Olá, {nome}, você é maior de idade.")
+else:
+    print(f"Olá, {nome}, você é menor de idade.")
